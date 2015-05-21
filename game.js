6c0bdcdfcb52631bd03b78424ae974192f3e3547
Oleksandr <alexanchik@mail.com>
2015-05-20 23:27:02 -0400

    added more code

--- game.js
+++ game.js
@@ -1,4 +1,23 @@
 if(jQuery){
+
+var checkAnswers = function(){
+var answerString = "";
+var answers = $(":checked");
+answers.each(function(i){
+answerString = answerString + answers[i].value;
+});
+$(":checked").each(function(i){
+var answerString = answerString + answers[i].value;
+});
+checkIfCorrect(answerString);
+};
+var checkIfCorrect = function(theString{
+if(parseInt(theString,16) === 811124566973){
+$("body").addClass("correct");
+$("h1").text("You Win!);
+$("canvas").show();
+}
+};
 $("#question1").show();
 };
 
